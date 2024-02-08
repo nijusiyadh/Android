8e70c6e8edafc699e82e9d196f3f39477f00d666
nijusiyadh <128395678+nijusiyadh@users.noreply.github.com>
2024-02-08 11:50:55 +0530

    Create NewActivity.java

---
+++ intent/NewActivity.java
@@ -0,0 +1,13 @@
+package com.example.intent;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NewActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new);
+    }
+}
